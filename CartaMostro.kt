cb133ee52cae215e99f8e56598c912320ec92f36
ZahirToufic <95553892+ZahirToufic@users.noreply.github.com>
2026-03-12 19:03:06 -0400

    Update CartaMostro.kt

--- CartaMostro.kt
+++ CartaMostro.kt
@@ -1,16 +1,21 @@
 //conviene que sea data o solo class? revisar si las data pueden tener funciones dentro
-data class CartaMostro(name: String, lvl: Int, pwr: Int, att: String) {
-    companion object val atributos: Array<String> = ["AGUA", "FUEGO", "VIENTO" "TIERRA", "LUZ", "OSCURIDAD", "DIVINO"]
-    companion object var names: List<String> = []
-
-    init {
-    require(lvl in 1..12) {"El nivel del Mostro debe estar entre 1 y 12"}
-    require(pwr % 50 == 0) {"Poder debe ser un múltiplo de 50"}
-    require(att in atributos) {"No se reconoce el atributo"}
-    require(name !in names) {"Ya hay un Mostro con este nombre"}
-
-    names.add(name)
+data class CartaMostro(val name: String, val lvl: Int, val pwr: Int, val att: String) {
+    companion object { 
+    	//val atributos: Array<String> = ["AGUA", "FUEGO", "VIENTO", "TIERRA", "LUZ", "OSCURIDAD", "DIVINO"]
+    	val atributos = arrayOf("AGUA", "FUEGO", "VIENTO", "TIERRA", "LUZ", "OSCURIDAD", "DIVINO")
+    	//var names: List<String> = []
+    	val names = mutableListOf<String>()
+    }
     
+    init {
+    	require(lvl in 1..12) {"El nivel del Mostro debe estar entre 1 y 12"}
+    	require(pwr > 0 && pwr % 50 == 0) {"Poder debe ser un múltiplo de 50"}
+    	require(att in atributos) {"No se reconoce el atributo"}
+    	require(name !in names) {"Ya hay un Mostro con este nombre"}
+
+    	names.add(name)
+    
+    }
 }
 
 /*    fun share(mostro1, mostro2: CartaMostro): Boolean {
